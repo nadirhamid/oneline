e711eacabfe47d2f4ae1de439704e330a569aa21
Nadir Hamid <matrix.nad@gmail.com>
2015-08-01 20:48:44 +0000

    updates to deprecated pack_message and parse_message

--- src/oneline/ol.py
+++ src/oneline/ol.py
@@ -411,19 +411,23 @@ def unicodeAll(dictionaryorlist):
 
 ## deprecated in 0.7.5
 def parse_message(message):
-  literal = ast.literal_eval(message.__str__())
-  return bsonlib.loads(bytearray(literal).__str__())
+  if isinstance(message,str):
+    literal = ast.literal_eval(message.__str__())
+    return bsonlib.loads(bytearray(literal).__str__())
+  return message
 
 ## opposite parse_message
 ## deprecated in 0.7.5
 def pack_message(message):
-  if not 'order' in message.keys():
-    message['order'] = []
-  if not 'data' in message.keys():
-    message['data'] = []
+  if isinstance(message, dict):
+    if not 'order' in message.keys():
+      message['order'] = []
+    if not 'data' in message.keys():
+      message['data'] = []
 
-  bytes = map(ord, bsonlib.dumps(message)).__str__()
-  return bytes
+    bytes = map(ord, bsonlib.dumps(message)).__str__()
+    return bytes
+  return message
 
 def parse(message,module=None):
   return parse_message(message) 
